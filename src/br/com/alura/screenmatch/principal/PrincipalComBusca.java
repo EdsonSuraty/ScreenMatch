6337e5ae9ab5a67298e33be8fd54b007c7a7d98b
Edson Suraty <edinhosuraty@gmail.com>
2025-06-22 20:55:12 -0300

    feat: update project

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -2,16 +2,26 @@ package br.com.alura.screenmatch.principal;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
-        HttpClient client = HttpClient.newHttpClient();
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite um filme para busca");
+        var busca = leitura.nextLine();
 
+        String buscaCodificada = URLEncoder.encode(busca, StandardCharsets.UTF_8);
+
+        String endereco = "https://www.omdbapi.com/?s=" + buscaCodificada + "&apikey=295ec06b";
+        System.out.println(endereco);
+        HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?t=yu-gi-oh&apikey=295ec06b"))
+                .uri(URI.create(endereco))
                 .build();
 
         HttpResponse<String> response = client
